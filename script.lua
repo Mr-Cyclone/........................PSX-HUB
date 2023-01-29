a9e90c589d1f54d06b169464acbe511f0c979c48
SpartanPluton <123672734+SpartanPluton@users.noreply.github.com>
2023-01-29 23:32:26 +0100

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,6 +1,12 @@
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
 
 local Window = OrionLib:MakeWindow({Name = "PSX-HUB by SpartanPluton", HidePremium = false, SaveConfig = true, ConfigFolder = "SpartanPlutonFolder"})
+OrionLib:MakeNotification({
+	Name = "[PSX-HUB by SpartanPluton]",
+	Content = "Launching Success!",
+	Image = "rbxassetid://4483345998",
+	Time = 10
+})
 
 -- -- MAIN -- -- 
 
